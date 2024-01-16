dfed7ced77d1a4f331608f1adc14dc465b830a65
Karol <karolkaraszewski@gmail.com>
2024-01-16 19:57:44 +0100

    first task module 4 a)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Zadanie1 obiektZ1 = new Zadanie1();
-        obiektZ1.sumArray();
+      //  Zadanie1 obiektZ1 = new Zadanie1();
+      //  obiektZ1.sumArray();
     }
 }
