6a52a820d15ac159428bb5195d013d6d5dbd2c28
Karol <karolkaraszewski@gmail.com>
2024-01-20 18:14:13 +0100

    update. module 4 without task 2

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Student implements Comparable<Student> {
     private int age;
     private int height;
@@ -13,47 +9,6 @@ public class Student implements Comparable<Student> {
         this.height = height;
         this.name = name;
         this.lastName = lastName;
-    }
-
-    public void sortStudent() {
-
-        List<Student> listOfStudents = new ArrayList<>();
-
-        Student student1 = new Student(24, 180, "John", "Doe5");
-        Student student2 = new Student(33, 179, "Roger", "Doe4");
-        Student student3 = new Student(12, 178, "Steven", "Doe3");
-        Student student4 = new Student(121, 177, "Mike", "Doe2");
-        Student student5 = new Student(200, 176, "Tony", "Doe1");
-
-        listOfStudents.add(student1);
-        listOfStudents.add(student2);
-        listOfStudents.add(student3);
-        listOfStudents.add(student4);
-        listOfStudents.add(student5);
-
-        System.out.println("Before sorting");
-        for (Student student : listOfStudents) {        //nie wiem czemu tak? - dopytać raz jeszcze!!!
-            System.out.println(student.getName() + " " + student.getLastName() + " " + student.getAge() + " " + student.getHeight());
-        }
-
-        Collections.sort(listOfStudents);
-        System.out.println(" ");
-        ;
-
-        System.out.println("After sorting by last name:");
-        for (Student student : listOfStudents) {
-            System.out.println(student.getName() + " " + student.getLastName() + " " + student.getAge() + " " + student.getHeight());
-        }
-        CompareByAge comparator = new CompareByAge();
-        Collections.sort(listOfStudents, comparator);
-        System.out.println(" ");
-        ;
-
-        System.out.println("After sorting by last name and age:");
-        for (Student student : listOfStudents) {
-            System.out.println(student.getName() + " " + student.getLastName() + " " + student.getAge() + " " + student.getHeight());
-        }
-
     }
 
     public int getAge() {
--- test/TestClassZadanie4.java
+++ test/TestClassZadanie4.java
@@ -1,16 +1,16 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class TestClassZadanie4 {
 
 
-
     @Test
-    public void areEqual() {
+    public void areThoseEqual() {
 
-        List<Student> listOfStudentsBefore = new ArrayList<>();
 
         Student student1 = new Student(24, 180, "John", "Doe5");
         Student student2 = new Student(33, 179, "Roger", "Doe4");
@@ -18,13 +18,32 @@ public class TestClassZadanie4 {
         Student student4 = new Student(121, 177, "Mike", "Doe2");
         Student student5 = new Student(200, 176, "Tony", "Doe1");
 
-        listOfStudentsBefore.add(student1);
-        listOfStudentsBefore.add(student2);
-        listOfStudentsBefore.add(student3);
-        listOfStudentsBefore.add(student4);
-        listOfStudentsBefore.add(student5);
+        List<Student> listOfStudentsNotSorted = new ArrayList<>();
 
-        Assertions.assertArrayEquals(listOfStudentsBefore,
+        listOfStudentsNotSorted.add(student1);
+        listOfStudentsNotSorted.add(student2);
+        listOfStudentsNotSorted.add(student3);
+        listOfStudentsNotSorted.add(student4);
+        listOfStudentsNotSorted.add(student5);
+
+        List<Student> listOfStudentsSorted = new ArrayList<>();
+
+        listOfStudentsSorted.add(student3);
+        listOfStudentsSorted.add(student1);
+        listOfStudentsSorted.add(student2);
+        listOfStudentsSorted.add(student4);
+        listOfStudentsSorted.add(student5);
+
+        Assertions.assertNotEquals(listOfStudentsSorted, listOfStudentsNotSorted);
+
+        Collections.sort(listOfStudentsNotSorted);
+        CompareByAge comparator = new CompareByAge();
+        Collections.sort(listOfStudentsNotSorted, comparator);
+
+        Assertions.assertEquals(listOfStudentsSorted, listOfStudentsNotSorted);
+
+        // potrzebuje to przeanalizowac krok po kroku, mimo ze to działa w zasadzie
+
     }
 
 }
